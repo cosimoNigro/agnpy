a6c99df6f74dd876d0ec80d5e54744a1e9e8ab4e
cosimoNigro <cosimonigro2@gmail.com>
2020-01-06 17:06:13 +0100

    API not generated in readthedocs, fix n.5

--- setup.py
+++ setup.py
@@ -12,11 +12,16 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cosimoNigro/agnpy",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("agnpy"),
+    package_dir={"": "agnpy"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "astropy",
+        "numpy"
     ],
     python_requires='>=3.7',
 )
