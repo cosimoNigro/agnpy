6478e36c7e7e1773532d177673a6e0638bdfd740
cosimoNigro <cosimonigro2@gmail.com>
2020-02-13 12:19:39 +0100

    fixed problemw with pypi

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agnpy",
-    version="0.0.2.1",
+    version="0.0.2.2",
     author="Cosimo Nigro",
     author_email="cosimonigro2@gmail.com.com",
     description="Modelling jetted Active Galactic Nuclei radiative processes with python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cosimoNigro/agnpy",
-    packages=setuptools.find_packages("agnpy"),
-    package_dir={"": "agnpy"},
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
