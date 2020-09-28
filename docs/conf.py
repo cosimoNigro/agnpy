17c0100379df066e5d9916ee2d01bf8ed1097497
cosimoNigro <cosimonigro2@gmail.com>
2020-09-28 11:23:38 +0200

    release 0.0.7.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2019, Cosimo Nigro"
 author = "Cosimo Nigro"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.7"
+release = "0.0.7.2"
 
 
 # -- General configuration ---------------------------------------------------
