0fe63c3428e9969bb1b15bdc3dd7d2fbf70bcb0e
cosimoNigro <cosimonigro2@gmail.com>
2020-10-20 18:45:38 +0200

    still overflowing for Compton on Disk

--- agnpy/utils/math.py
+++ agnpy/utils/math.py
@@ -11,7 +11,7 @@ def log(x):
     values = np.clip(x, float_tiny, float_max)
     return np.log(values)
 
-def trapz_loglog(y, x, axis=-1, intervals=False):
+def trapz_loglog(y, x, axis=0):
     """
     Integrate along the given axis using the composite trapezoidal rule in
     loglog space.
@@ -42,46 +42,43 @@ def trapz_loglog(y, x, axis=-1, intervals=False):
         x = x.value
     except AttributeError:
         x_unit = 1.0
+    
+    slice_low = [slice(None)] * y.ndim
+    slice_up = [slice(None)] * y.ndim
+    # multi-dimensional equivalent of x_low = x[:-1]
+    slice_low[axis] = slice(None, -1)
+    # multi-dimensional equivalent of x_up = x[1:]
+    slice_up[axis] = slice(1, None)
 
-    y = np.asanyarray(y)
-    x = np.asanyarray(x)
+    slice_low = tuple(slice_low)
+    slice_up = tuple(slice_up)
 
-    slice1 = [slice(None)] * y.ndim
-    slice2 = [slice(None)] * y.ndim
-    slice1[axis] = slice(None, -1)
-    slice2[axis] = slice(1, None)
-
-    slice1 = tuple(slice1)
-    slice2 = tuple(slice2)
-
+    # reshape x to be broadcasted with y
     if x.ndim == 1:
         shape = [1] * y.ndim
         shape[axis] = x.shape[0]
         x = x.reshape(shape)
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        # Compute the power law indices in each integration bin
-        b = np.log10(y[slice2] / y[slice1]) / np.log10(x[slice2] / x[slice1])
-
-        # if local powerlaw index is -1, use \int 1/x = log(x); otherwise use
-        # normal powerlaw integration
-        trapzs = np.where(
-            np.abs(b + 1.0) > 1e-10,
-            (
-                y[slice1]
-                * (x[slice2] * (x[slice2] / x[slice1]) ** b - x[slice1])
-            )
-            / (b + 1),
-            x[slice1] * y[slice1] * np.log(x[slice2] / x[slice1]),
-        )
-
-    tozero = (y[slice1] == 0.0) + (y[slice2] == 0.0) + (x[slice1] == x[slice2])
-    trapzs[tozero] = 0.0
-
-    if intervals:
-        return trapzs * x_unit * y_unit
-
-    ret = np.add.reduce(trapzs, axis) * x_unit * y_unit
-
-    return ret
+        
+    x_low = x[slice_low] 
+    x_up = x[slice_up]
+    y_low = y[slice_low]
+    y_up = y[slice_up]
+    
+    log_x_low = log(x_low)
+    log_x_up = log(x_up)
+    log_y_low = log(y_low)
+    log_y_up = log(y_up)
+    
+    # index in the bin
+    m = (log_y_low - log_y_up) / (log_x_low - log_x_up)
+    vals = y_low / (m + 1) * (x_up * (x_up / x_low) ** m - x_low)
+    
+    # value of y very close to zero will make m large and explode the exponential
+    tozero = (
+        np.isclose(y_low, 0, atol=0, rtol=1e-10) +
+        np.isclose(y_up, 0, atol=0, rtol=1e-10) +
+        np.isclose(x_low, x_up, atol=0, rtol=1e-10)
+    )
+    vals[tozero] = 0.0
+    
+    return np.add.reduce(vals, axis) * x_unit * y_unit
