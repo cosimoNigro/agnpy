a122aaf9999592663a8a6a54046bf232d3542b31
cosimoNigro <cosimonigro2@gmail.com>
2021-12-14 12:15:55 +0100

    prepared release 0.1.6

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2019, Cosimo Nigro"
 author = "Cosimo Nigro"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.4"
+release = "0.1.6"
 
 
 # -- General configuration ---------------------------------------------------
