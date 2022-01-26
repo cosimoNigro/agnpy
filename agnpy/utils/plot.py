746b55377000e7131d84b1d6138f3be55c242232
cosimoNigro <cosimonigro2@gmail.com>
2022-01-26 14:08:59 +0100

    added default maptplotlibrc called by the load_mpl_rc function

--- agnpy/utils/plot.py
+++ agnpy/utils/plot.py
@@ -1,36 +1,16 @@
 # plotting utilities for agnpy
+from pathlib import Path
 import matplotlib.pyplot as plt
 
 # axes labels
 sed_x_label = r"$\nu\,/\,\mathrm{Hz}$"
 sed_y_label = r"$\nu F_{\nu}\,/\,(\mathrm{erg}\,\mathrm{cm}^{-2}\,\mathrm{s}^{-1})$"
 
-mpl_linewidth = 1.6
-mpl_rc = {
-    "figure.autolayout": True,
-    "figure.dpi": 150,
-    "font.size": 12,
-    "lines.linewidth": mpl_linewidth,
-    "axes.grid": True,
-    "grid.linestyle": ":",
-    "grid.linewidth": 1.4,
-    "axes.linewidth": mpl_linewidth,
-    "xtick.major.size": 7,
-    "xtick.major.width": mpl_linewidth,
-    "xtick.minor.size": 4,
-    "xtick.minor.width": mpl_linewidth,
-    "xtick.minor.visible": True,
-    "ytick.major.size": 7,
-    "ytick.major.width": mpl_linewidth,
-    "ytick.minor.size": 4,
-    "ytick.minor.width": mpl_linewidth,
-    "ytick.minor.visible": True,
-}
-
 
 def load_mpl_rc():
-    """use the custom matplotlib rc"""
-    plt.style.use(mpl_rc)
+    """use the custom matplotlibrc in this subdirectory"""
+    mpl_rc = Path(__file__).parent
+    plt.style.use(mpl_rc / "matplotlibrc")
 
 
 def plot_sed(nu, sed, ax=None, **kwargs):
