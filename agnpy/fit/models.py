c624147c42e01c8c5143e59fbe368d4225a22661
cosimoNigro <cosimonigro2@gmail.com>
2022-08-03 12:08:10 +0200

    added option to raise an error in case of unavailable backend

--- agnpy/fit/models.py
+++ agnpy/fit/models.py
@@ -2,7 +2,10 @@ from .gammapy_wrapper import (
     SynchrotronSelfComptonSpectralModel,
     ExternalComptonSpectralModel,
 )
-from .sherpa_wrapper import SynchrotronSelfComptonRegriddableModel1D
+from .sherpa_wrapper import (
+    SynchrotronSelfComptonRegriddableModel1D,
+    ExternalComptonRegriddableModel1D,
+)
 
 
 class SynchrotronSelfComptonModel:
@@ -13,6 +16,10 @@ class SynchrotronSelfComptonModel:
             return SynchrotronSelfComptonSpectralModel(n_e, ssa)
         elif backend == "sherpa":
             return SynchrotronSelfComptonRegriddableModel1D(n_e, ssa)
+        else:
+            raise ValueError(
+                f"{backend} is not an available backend, try gammapy or sherpa"
+            )
 
 
 class ExternalComptonModel:
@@ -21,3 +28,9 @@ class ExternalComptonModel:
     def __new__(cls, n_e, targets, ssa=False, backend="gammapy"):
         if backend == "gammapy":
             return ExternalComptonSpectralModel(n_e, targets, ssa)
+        if backend == "sherpa":
+            return ExternalComptonRegriddableModel1D(n_e, targets, ssa)
+        else:
+            raise ValueError(
+                f"{backend} is not an available backend, try gammapy or sherpa"
+            )
