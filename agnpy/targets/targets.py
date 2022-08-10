tried to fix the normalisation of the DT and disk black bodies. Adopted analytical result for DT, and a numerical integration on a large grid for the disk.

agnpy/targets/targets.py
@@ -147,6 +147,9 @@ lines_dictionary = {
     },
 }
 
+# array of frequencies to numerically integrate the multi-T disk black body
+nu_to_integrate = np.logspace(3, 21, 100) * u.Hz
+
 
 class CMB:
     """Cosmic Microwave Background radiation, approximated as an isotropic
@@ -396,17 +399,22 @@ class SSDisk:
         """Evaluate a normalised, multi-temperature black body SED as in
         :func:`~targets.SSDisk.evaluate_multi_T_bb_sed`, but the integral luminosity
         is set to be equal to `L_disk`."""
-        sed_disk = SSDisk.evaluate_multi_T_bb_sed(
-            nu, z, M_BH, m_dot, R_in, R_out, d_L, mu_s
+        # it is difficult to integrate numerically the multi-T BB of the disk
+        # to get the total luminosity, let us integrate it numerically
+        # over a large range of frequencies
+        F_nu_norm = (
+            SSDisk.evaluate_multi_T_bb_sed(
+                nu_to_integrate, z, M_BH, m_dot, R_in, R_out, d_L, mu_s
+            )
+            / nu_to_integrate
         )
         # renormalise, the factor 2 includes the two sides of the Disk
-        L = 2 * (np.trapz(sed_disk / nu, nu) * 4 * np.pi * d_L ** 2).to("erg s-1")
-        # if the SED is 0, avoid re-normalising with its integral (0 erg s-1)
-        if u.isclose(L, 0 * u.Unit("erg s-1")):
-            return sed_disk
-        else:
-            norm = (L_disk / L).to_value("")
-            return norm * sed_disk
+        A = 4 * np.pi * d_L ** 2
+        L = 2 * (np.trapz(F_nu_norm, nu_to_integrate) * A).to("erg s-1")
+        norm = (L_disk / L).to_value("")
+        return norm * SSDisk.evaluate_multi_T_bb_sed(
+            nu, z, M_BH, m_dot, R_in, R_out, d_L, mu_s
+        )
 
     def sed_flux(self, nu, z, mu_s=1):
         r"""evaluate the multi-temperature black body SED for this SS Disk, refer
@@ -596,14 +604,10 @@ class RingDustTorus:
         """evaluate the torus black-body SED such that its integral luminosity
         is equal to the torus luminosity (`xi_dt * L_disk`)"""
         sed_dt = RingDustTorus.evaluate_bb_sed(nu, z, T_dt, R_dt, d_L)
-        # renormalise
-        L = (np.trapz(sed_dt / nu, nu) * 4 * np.pi * d_L ** 2).to("erg s-1")
-        # if the SED is 0, avoid re-normalising with its integral (0 erg s-1)
-        if u.isclose(L, 0 * u.Unit("erg s-1")):
-            return sed_dt
-        else:
-            norm = (L_dt / L).to_value("")
-            return norm * sed_dt
+        # renormalise, total luminosity from theory (Stefan-Boltzmann Law)
+        L_tot = 4 * np.pi * R_dt ** 2 * sigma_sb * T_dt ** 4
+        norm = (L_dt / L_tot).to_value("")
+        return norm * sed_dt
 
     def sed_flux(self, nu, z):
         """evaluate the black-body SED for the Dust Torus"""