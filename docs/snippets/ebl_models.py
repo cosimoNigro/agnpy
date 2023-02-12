7912fdce48720010b1cf70069419f72fe9b7eb11
cosimoNigro <cosimonigro2@gmail.com>
2023-02-12 17:31:12 +0100

    fixed also snippet in the docs showing EBL abosrption

--- docs/snippets/ebl_models.py
+++ docs/snippets/ebl_models.py
@@ -7,20 +7,20 @@ from agnpy.utils.plot import load_mpl_rc
 
 
 # redshift and array of frequencies over which to evaluate the absorption
-z = 1
-nu = np.logspace(15, 25) * u.Hz
-
+z = 0.5
+nu = np.logspace(15, 28, 200) * u.Hz
+E = nu.to("eV", equivalencies=u.spectral())
 
 load_mpl_rc()
 
 for model in ["franceschini", "dominguez", "finke", "saldana-lopez"]:
     ebl = EBL(model)
     absorption = ebl.absorption(z, nu)
-    plt.loglog(nu, absorption, label=model)
+    plt.loglog(E, absorption, label=model)
 
-plt.xlabel(r"$\nu\,/\,{Hz}$")
+plt.xlabel(r"$E\,/\,{\rm eV}$")
 plt.title("EBL absorption at z=1")
 plt.ylabel("absorption")
+plt.ylim([1e-4, 2])
 plt.legend()
-plt.ylim([1e-5, 2])
 plt.show()
