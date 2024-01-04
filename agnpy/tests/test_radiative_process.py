a34f12e9e85837f83fd75665234ef4c71e67ab4b
cosimoNigro <cosimonigro2@gmail.com>
2024-01-04 16:53:50 +0100

    improved tests on the RadiativeProcess class

--- agnpy/tests/test_radiative_process.py
+++ agnpy/tests/test_radiative_process.py
@@ -1,84 +1,78 @@
 import numpy as np
-import math
 import astropy.units as u
-from astropy.constants import m_e, h
+from astropy.constants import m_e
 from astropy.coordinates import Distance
 from agnpy.spectra import PowerLaw
 from agnpy.emission_regions import Blob
 from agnpy.synchrotron import Synchrotron
-from agnpy.radiation.radiative_process import RadiativeProcess
+from agnpy.radiative_process import RadiativeProcess
 
 
-def _estimate_powerlaw_integral(x_values, y_values):
-    slope, intercept = np.polyfit(np.log10(x_values.value), np.log10(y_values.value), 1)
-    power = slope + 1
-    estimated_integral = (10 ** intercept) * (
-            x_values[-1].value ** power / power - x_values[0].value ** power / power)
-    return estimated_integral
+def estimate_powerlaw_integral(x, y):
+    """Fit a power law to the data, then estimate its integral."""
+    index, log10_norm = np.polyfit(np.log10(x), np.log10(y), 1)
+    norm = 10**log10_norm
+    integral_index = index + 1
+    integral = (
+        norm / (integral_index) * (x[-1] ** integral_index - x[0] ** integral_index)
+    )
+    return integral
 
 
-def _sample_synchrotron_model():
-    """The model based on the synchrotron example from the agnpy documentation."""
+def create_synchrotron_model():
+    """A simple synchrotron model to be used for test purpoises.
+    Based on the synchrotron example from the documentation."""
     R_b = 1e16 * u.cm
-    n_e = PowerLaw.from_total_energy(1e48 * u.erg, 4 / 3 * np.pi * R_b ** 3, p=2.8, gamma_min=1e2,
-                                     gamma_max=1e7, mass=m_e)
-    blob = Blob(R_b, z=Distance(1e27, unit=u.cm).z, delta_D=10, Gamma=10, B=1 * u.G, n_e=n_e)
+    V_b = 4 / 3 * np.pi * R_b**3
+    z = Distance(1e27, unit=u.cm).z
+    B = 1 * u.G
+    delta_D = 10
+    n_e = PowerLaw.from_total_energy(
+        1e48 * u.erg, V_b, p=2.8, gamma_min=1e2, gamma_max=1e7, mass=m_e
+    )
+    blob = Blob(R_b=R_b, z=z, delta_D=delta_D, Gamma=delta_D, B=B, n_e=n_e)
     synch = Synchrotron(blob)
     return synch
 
 
-class FlatSedGenerator(RadiativeProcess):
-    """A dummy generator returning flat (constant) flux, of the same value across the whole spectrum."""
-    def __init__(self, flat_value):
-        self.flat_value = flat_value
+class FlatFNuSedGenerator(RadiativeProcess):
+    """A dummy generator returning a flat (constant) SED in F_nu."""
+
+    def __init__(self, F_nu):
+        # the constant F_nu value
+        self.F_nu = F_nu
 
     def sed_flux(self, nu):
-        return np.full(fill_value=self.flat_value, shape=nu.shape, dtype=np.float64) * (
-                    u.erg / (u.cm ** 2 * u.s * u.Hz)) * nu
+        F_nu = np.ones_like(nu).value * self.F_nu
+        return (nu * F_nu).to("erg cm-2 s-1")
 
 
 class TestSedIntegration:
-
-    def test_flat_integral(self):
+    def test_flat_energy_flux_integral(self):
         """Integrate over flat SED (Fnu equal to 1.0 for all frequencies)."""
         nu_min = 10 * u.Hz
-        nu_max = 10 ** 20 * u.Hz
-        flux = FlatSedGenerator(1.0).integrate_flux(nu_min, nu_max)
-        assert flux == 1e+20 * u.erg / (u.cm ** 2 * u.s)
+        nu_max = 1e9 * u.Hz
+        F_nu = 1 * u.Unit("erg cm-2 s-1 Hz-1")
+        energy_flux_integral = FlatFNuSedGenerator(F_nu).energy_flux_integral(
+            nu_min, nu_max
+        )
+        assert u.isclose(energy_flux_integral, 1e9 * u.Unit("erg cm-2 s-1"))
 
+    def test_synchrotron_integrals(self):
+        """Create an example synchrotron SED, fit the flux with a power-law part
+        and compare its integral."""
+        synch = create_synchrotron_model()
+        nu = np.logspace(13, 20, 50) * u.Hz
+        energy = nu.to("eV", equivalencies=u.spectral())
 
-    def test_synchrotron_energy_flux_integral(self):
-        """Integrate over sample synchrotron flux and compare with the value estimated using the power law integral."""
-        synch = _sample_synchrotron_model()
+        energy_flux_integral = synch.energy_flux_integral(nu[0], nu[-1]).value
+        flux_integral = synch.flux_integral(nu[0], nu[-1]).value
 
-        nu_min_log = 13
-        nu_max_log = 20
-        nu_min = 10 ** nu_min_log * u.Hz
-        nu_max = 10 ** nu_max_log * u.Hz
+        F_nu = synch.sed_flux(nu) / nu
+        dphidE = synch.diff_energy_flux(nu)
 
-        nu = np.logspace(nu_min_log, nu_max_log) * u.Hz
-        Fnu = synch.sed_flux(nu) / nu
-        estimated_integral = _estimate_powerlaw_integral(nu, Fnu)
+        energy_flux_integral_from_fit = estimate_powerlaw_integral(nu.value, F_nu.value)
+        flux_integral_from_fit = estimate_powerlaw_integral(energy.value, dphidE.value)
 
-        actual_integral = synch.integrate_flux(nu_min, nu_max).value
-
-        assert math.isclose(actual_integral, estimated_integral, rel_tol=0.05)
-
-    def test_synchrotron_photon_flux_integral(self):
-        """Integrate over sample synchrotron photon flux and compare with the value estimated using the power law integral."""
-        synch = _sample_synchrotron_model()
-
-        nu_min_log = 13
-        nu_max_log = 20
-        nu_min = 10 ** nu_min_log * u.Hz
-        nu_max = 10 ** nu_max_log * u.Hz
-
-        nu = np.logspace(nu_min_log, nu_max_log) * u.Hz
-        flux = synch.diff_photon_flux(nu)
-        energies_eV = nu.to("eV", equivalencies=u.spectral())
-
-        estimated_integral = _estimate_powerlaw_integral(energies_eV, flux)
-
-        actual_integral = synch.integrate_photon_flux(nu_min, nu_max).value
-
-        assert math.isclose(actual_integral, estimated_integral, rel_tol=0.05)
+        assert u.isclose(energy_flux_integral, energy_flux_integral_from_fit, rtol=5e-2)
+        assert u.isclose(flux_integral, flux_integral_from_fit, rtol=5e-2)
