dec0da4928593b206ecc3bd85db8b3eb44e959e8
pwit07 <ul0255264@gmail.com>
2026-01-13 14:59:04 +0100

    Added automatic tests

--- agnpy/photo_meson/tests/test_kernels.py
+++ agnpy/photo_meson/tests/test_kernels.py
@@ -1,8 +1,25 @@
 # test the kernels
-import numpy as np
+# import numpy as np
 from pathlib import Path
 import pytest
-from agnpy.photo_meson.kernels import PhiKernel
+# from agnpy.photo_meson.kernels import PhiKernel
+
+import numpy as np
+import astropy.units as u
+from astropy.constants import c, h, m_p
+from agnpy.photo_meson.kernels import PhiKernel, secondaries, eta_0
+from agnpy.utils.math import axes_reshaper, ftiny, fmax, log10
+from agnpy.utils.conversion import mpc2
+
+# to be used only for the the validation
+from astropy.modeling.models import BlackBody
+import matplotlib.pyplot as plt
+from agnpy.emission_regions import Blob
+from agnpy.spectra import PowerLaw, ExpCutoffPowerLaw
+from agnpy.photo_meson.photo_meson import PhotoMesonProduction
+from agnpy.targets.targets import CMB
+
+
 from agnpy.utils.validation_utils import (
     make_comparison_plot,
     extract_columns_sample_file,
@@ -35,7 +52,7 @@ class TestKernels:
 
         phi = PhiKernel(particle)
         eta = float(eta_eta0) * eta_0
-        phi_agnpy = phi(eta, x_ref)
+        phi_agnpy = phi(eta, x_ref).to_value("cm3 s-1")
 
         # comparison plot
         x_max_comparison = 0.1 if eta_eta0 == "1.5" else 0.2
@@ -57,4 +74,83 @@ class TestKernels:
         # requires that the SED points deviate less than 25% from the figure
         assert check_deviation(
             x_ref, x_ref * phi_agnpy, x_phi_ref, 0.25, x_range=x_range
+        )
+    @pytest.mark.parametrize(
+        "particle", ["gamma", "electron", "positron", "muon_neutrino", "muon_antineutrino", "electron_neutrino", "electron_antineutrino"]
+    )
+    @pytest.mark.parametrize("fig_number", ["14", "15", "16", "17"])
+    def test_spectrum(self, particle, fig_number):
+        """Test the interpolations against the plots presented in the [KelnerAharonian2008]_ paper."""
+        
+        factor = 1.0e3
+
+        if fig_number == "14":
+            factor = 1e-1
+        if fig_number == "15":
+            factor = 1e0
+        if fig_number == "16":
+            factor = 1e1
+
+        # Blob with proton population
+        E_star = 3e20 * u.Unit("eV")
+        gamma_star = (E_star / mpc2).to_value("")
+        # p_p, gamma_c, gamma_min, gamma_max = 2, 1e3*gamma_star, (1.0*u.Unit("GeV")/mpc2).to_value(""), 1e6*gamma_star
+        
+        n_p = ExpCutoffPowerLaw.from_total_energy_density(
+            1.0*u.Unit("erg/cm3"),
+            mass = m_p,
+            p = 2,
+            gamma_c = factor*gamma_star, # change fig_number!
+            gamma_min = (1.0*u.Unit("GeV")/mpc2).to_value(""),
+            gamma_max = 1e6*gamma_star
+            )
+
+        blob = Blob(n_p = n_p)
+        # print ("Proton energy density = ", blob.u_p)
+
+        cmb = CMB(z = 0.0)
+        cmb_target = lambda nu: cmb.du_dnu(nu)
+
+        E_i, spectrum_ref = np.genfromtxt(f"{data_dir}/photo_meson/kelner_aharonian_2008/fig{fig_number}_values/{particle}.txt", 
+                          dtype="float", 
+                          delimiter=";",
+                          comments="#", 
+                          usecols=(0, 1), 
+                          unpack="True",
+                          converters={0: lambda x: float(x.replace(',', '.')),
+                                      1: lambda x: float(x.replace(',', '.'))})
+        # print(E_i,spectrum)
+        E_i = np.power(10,E_i)*u.Unit("eV")
+        spectrum_ref = np.power(10,spectrum_ref)#*u.Unit("cm-3 s-1")
+
+        pmp_cmb = PhotoMesonProduction(blob, cmb_target)
+
+        # E_i = np.logspace(17, 21, 100) * u.Unit("eV")
+        spectrum = ((pmp_cmb.evaluate_spectrum(E_i, particle = particle)*E_i).to_value(f"cm-3 s-1"))
+        # print(spectrum)
+
+        E_i = E_i.to_value("eV")
+
+        E_range = [E_i.min(), E_i.max()]
+
+        # # comparison plot
+        # x_max_comparison = 0.1 if eta_eta0 == "1.5" else 0.2
+        # x_range = [2e-4, x_max_comparison]
+        # make_comparison_plot(
+        #     x=x_ref,
+        #     y_comp=x_ref * phi_agnpy,
+        #     y_ref=x_phi_ref,
+        #     comp_label="agnpy",
+        #     ref_label="Kelner and Aharonian (2008)",
+        #     fig_title=r"$\phi$" + f" {particle.replace('_', ' ')}",
+        #     fig_path=f"{figures_dir}/phi_comparison_particle_{particle}_eta_{eta_eta0}_eta0.png",
+        #     plot_type="custom",
+        #     x_label=r"$x = E_{\gamma} / E_{\rm p}$",
+        #     y_label=r"$x \phi(\eta, x)$",
+        #     y_range=None,
+        #     comparison_range=x_range,
+        # )
+        # requires that the SED points deviate less than 25% from the figure
+        assert check_deviation(
+            E_i, spectrum, spectrum_ref, 0.50, x_range=E_range
         )
