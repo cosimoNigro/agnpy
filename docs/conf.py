9b6993e7363db3424c34b4979ef2ad30df1291e4
cosimoNigro <cosimonigro2@gmail.com>
2026-02-03 14:15:27 +0100

    bumping up python version to fix readthedocs failure in building

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "agnpy"
-copyright = "2022, Cosimo Nigro"
-author = "Cosimo Nigro"
+copyright = "2025, agnpy developers"
+author = "agnpy developers"
 
 # The full version, including alpha/beta/rc tags
 release = "0.5.0"
