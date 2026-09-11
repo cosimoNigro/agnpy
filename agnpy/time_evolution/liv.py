f8034586353f9a7856072e6e4a95628079c70310
Ugo <ugo.pensec@hotmail.fr>
2026-09-11 15:36:35 +0200

    Some corrections to the class

--- agnpy/time_evolution/liv.py
+++ agnpy/time_evolution/liv.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 import astropy.units as u
 from astropy.constants import h
+from agnpy.time_evolution import TimeEvolution, synchrotron_loss
 
 
 # Idea = take the lambda parameter as input, calculate the delay induced on each flux point (?) and either calculate the spectrum as a function of time or introduce a parameter of the class representing the time after the start of the inpulse
@@ -20,12 +21,12 @@ class LIVdelay:
     def __init__(self, lambda_liv, n_liv, blob, nu_syn, synch, time_step, steps):
         self.lambda_liv = lambda_liv
         self.n_liv = n_liv
-        self.time_evoluted_seds = np.array() # so a 2D matrix, containing the spectral evolution as a function of time
         self.blob = blob
         self.synch = synch
         self.time_step = time_step
         self.steps = steps
         self.nu_syn = nu_syn
+        self.time_evoluted_seds = self.calculate_time_evoluted_seds() # so a 2D matrix, containing the spectral evolution as a function of time
 
     def calculate_time_evoluted_seds(self):
         """
@@ -35,9 +36,8 @@ class LIVdelay:
         for i in range(self.steps):
             time_evolution = TimeEvolution(self.blob, self.time_step, synchrotron_loss(self.synch))
             time_evolution_result = time_evolution.evaluate()
-            time_evoluted_seds.append(synch.sed_flux(self.nu_syn))
-        self.time_evoluted_seds = np.array(time_evoluted_seds)
-        return self.time_evoluted_seds
+            time_evoluted_seds.append(self.synch.sed_flux(self.nu_syn))
+        return np.array(time_evoluted_seds)
 
     def interpolate_seds(self, nu_index, t):
         """
@@ -52,13 +52,13 @@ class LIVdelay:
         """
         Evaluates the SED for a given time of observation
         """
-        calculate_time_evoluted_seds()
-        time_evoluted_seds_liv = np.zeros(len(self.nu_syn))
-        for nu_index, nu in enumerate(nu_syn):
+        
+        time_evoluted_sed_liv = np.zeros(len(self.nu_syn))
+        for nu_index, nu in enumerate(self.nu_syn):
             time_obs_liv = time_obs + self.lambda_liv * (nu*h.to("TeV s"))**self.n_liv # change it to avg frequency in the bin
-            time_evoluted_seds_liv[nu_index] = interpolate_seds(self.steps, self.time_step, nu_index, time_obs_LIV, self.time_evoluted_seds)
+            time_evoluted_sed_liv[nu_index] = self.interpolate_seds(nu_index, time_obs_liv)
 
-        return time_evoluted_seds_liv
+        return time_evoluted_sed_liv
 
 
     # def functions that will be needed like Doppler boosting? 
